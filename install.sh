7ceae295902d01d31ba6a8347161d26128df9059
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-11-19 09:56:13 +0000

    no message

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir /emailcleaner123
+cd /emailcleaner123
+git clone https://github.com/whittinghamj/emailcleaner123.git . 
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /emailcleaner123 && git --git-dir=/emailcleaner123/.git pull origin master
